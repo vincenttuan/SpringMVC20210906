98c9d6d2a83c45b3444dd307873f33a5c6821e66
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-09-10 19:18:16 +0800

    Add files via upload

--- src/main/java/com/mvc/controller/Hello.java
+++ src/main/java/com/mvc/controller/Hello.java
@@ -1,5 +1,30 @@
 package com.mvc.controller;
 
-public class Hello {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping(value = "/hello")
+public class Hello {
+	
+	@RequestMapping(value = "/greet")
+	@ResponseBody
+	public String greet() {
+		return "Hello Greet !";
+	}
+	
+	/*
+	 * 路徑：/sayhi?name=vincent&age=18
+	 * 帶入：name 與 age 二個參數
+	 * */
+	@RequestMapping(value = "/sayhi")
+	@ResponseBody
+	public String sayHi(@RequestParam("name") String name, 
+			            @RequestParam("age") Integer age) {
+		
+		return "Hello " + name + ", " + age;
+	}
+	
 }
---
+++ src/main/java/com/mvc/controller/Hi.java
@@ -0,0 +1,17 @@
+package com.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/hi")
+public class Hi {
+	
+	@RequestMapping(value = "/greet")
+	@ResponseBody
+	public String greet() {
+		return "Hi Greet !";
+	}
+	
+}
