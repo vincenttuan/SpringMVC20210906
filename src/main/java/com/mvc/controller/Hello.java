6148f32fc1bcfd62572e09efc44d10a78feab76c
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-09-10 19:45:16 +0800

    Add files via upload

--- src/main/java/com/mvc/controller/Hello.java
+++ src/main/java/com/mvc/controller/Hello.java
@@ -1,6 +1,7 @@
 package com.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,10 +22,34 @@ public class Hello {
 	 * */
 	@RequestMapping(value = "/sayhi")
 	@ResponseBody
-	public String sayHi(@RequestParam("name") String name, 
+	public String sayHi(@RequestParam(value = "name", defaultValue = "unknown", required = false) String name, 
 			            @RequestParam("age") Integer age) {
 		
 		return "Hello " + name + ", " + age;
+	}
+	
+	/*
+	 * Lab:
+	 * 路徑：/bmi?h=170&w=60
+	 * 帶入：h 與 w 二個參數
+	 * 結果：bmi = 20.76
+	 * */
+	@RequestMapping(value = "/bmi")
+	@ResponseBody
+	public String bmi(@RequestParam("h") Double h, @RequestParam("w") Double w) {
+		double bmi = w / Math.pow(h/100, 2);
+		return String.format("bmi = %.2f", bmi);
+	}
+	
+	/*
+	 * PathVariable
+	 * 路徑：/exam/75 -> 結果：75 pass
+	 * 路徑：/exam/45 -> 結果：45 fail
+	 * */
+	@RequestMapping(value = "/exam/{score}")
+	@ResponseBody
+	public String exam(@PathVariable("score") Integer score) {
+		return score + " " + ((score >= 60)? "Pass" : "Fail");
 	}
 	
 }
