0c65cf750a356263333a2db30a85931e38f9b19e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-09-17 21:50:31 +0800

    Add files via upload

---
+++ src/main/java/com/mvc/controller/Hi.java
@@ -0,0 +1,50 @@
+package com.mvc.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/hi")
+public class Hi {
+	
+	@RequestMapping(value = "/{welcome}", method = {RequestMethod.POST, RequestMethod.GET})
+	@ResponseBody
+	public String greet(@PathVariable("welcome") String welcome, 
+			            @RequestParam("name") String name) {
+		// 解決路徑中文問題
+		// 預設的編碼是: ISO-8859-1
+		// 改變編碼: UTF-8
+		try {
+			welcome = new String(welcome.getBytes("ISO-8859-1"), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return welcome + " " + name + " !";
+	}
+	
+	@GetMapping(value = "/sayhi")
+	public ModelAndView sayhi() {
+		ModelAndView mav = new ModelAndView();
+		//mav.setViewName("/WEB-INF/view/sayhi.jsp");
+		mav.setViewName("sayhi"); // 於 springmvc-servlet.xml 中有定義 ViewResolver 標籤的寫法
+		mav.addObject("username", "John");
+		return mav;
+	}
+	
+	@GetMapping(value = "/sayhi2")
+	public String sayhi2(Model model) {
+		model.addAttribute("username", "Mary");
+		return "sayhi"; // 直接回傳的就是 view 的名字(字串)
+	}
+	
+}
---
+++ src/main/java/com/mvc/validator/ExamValidator.java
@@ -0,0 +1,40 @@
+package com.mvc.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.mvc.entity.Exam;
+
+@Component
+public class ExamValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// 判定當前 clazz 是不是 Exam 的類別
+		return Exam.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Exam exam = (Exam)target;
+		// 判定 id 不可以是 null 或空的
+		if(exam.getId() == null || exam.getId().trim().length() == 0) {
+			// field, errorCode, defaultMessage
+			// field : 要驗證的物件變數
+			// errorCode : 錯誤名稱(通常是指 errorMessages.properties 所設定的名稱)
+			// defaultMessage : 預設的錯誤訊息
+			errors.rejectValue("id", "exam.id.required", "id 不可空白");
+		}
+		if(exam.getName() == null || exam.getName().trim().length() == 0) {
+			errors.rejectValue("name", "exam.name.required", "請選擇考試代號");
+		}
+		if(exam.getSlot() == null || exam.getSlot().length == 0) {
+			errors.rejectValue("slot", "exam.slot.required", "請選擇考試時段");
+		}
+		if(exam.getPay() == null || exam.getPay().trim().length() == 0) {
+			errors.rejectValue("pay", "exam.pay.required", "請選擇繳費狀況");
+		}
+	}
+
+}
