7ad024f812dcf191153954c34302ae806c7c3e56
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-09-24 21:34:43 +0800

    Add files via upload

--- src/main/java/com/mvc/controller/ProductController.java
+++ src/main/java/com/mvc/controller/ProductController.java
@@ -2,16 +2,23 @@ package com.mvc.controller;
 
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.mvc.entity.products.Product;
 import com.mvc.service.ProductService;
 
 @Controller
 @RequestMapping(value = "/product")
+@SessionAttributes(value = {"groups", "products", "action"})
 public class ProductController {
 	
 	@Autowired
@@ -24,6 +31,16 @@ public class ProductController {
 		model.addAttribute("products", productService.query());
 		model.addAttribute("action", "save");
 		return "product";
+	}
+	
+	@PostMapping(value = "/save")
+	// 若要使用 JSR 303 驗證則要在驗證資料模組前面加上 @Valid 修飾
+	public String save(@Valid Product product, BindingResult result, Model model) {
+		if(result.hasErrors()) { // 是否有錯誤發生?
+			return "product"; // 將錯誤資訊帶給指定 jsp 頁面
+		}
+		productService.save(product);
+		return "redirect:/mvc/product/";
 	}
 	
 }
