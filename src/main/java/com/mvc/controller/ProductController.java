8219059ffcbb0334e3d82251b23fc6a14c7c4749
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-09-24 21:23:38 +0800

    Add files via upload

--- src/main/java/com/mvc/controller/ProductController.java
+++ src/main/java/com/mvc/controller/ProductController.java
@@ -2,22 +2,27 @@ package com.mvc.controller;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mvc.entity.products.Product;
+import com.mvc.service.ProductService;
 
 @Controller
 @RequestMapping(value = "/product")
 public class ProductController {
 	
+	@Autowired
+	private ProductService productService;
+	
 	@RequestMapping(value = {"/", "/input"})
 	public String index(Model model) {
 		model.addAttribute("product", new Product());
+		model.addAttribute("groups", productService.groups.values());
+		model.addAttribute("products", productService.query());
 		model.addAttribute("action", "save");
-		
-		
 		return "product";
 	}
 	
